44ea0ad05c37ad067ecf27d636075b55e6e974e4
mariorived <82894205+mariorived@users.noreply.github.com>
2021-06-17 20:39:47 +0200

    Update Usuario.java

--- src/main/java/clases/Usuario.java
+++ src/main/java/clases/Usuario.java
@@ -3,64 +3,80 @@ package clases;
 import java.util.Objects;
 
 public class Usuario {
-    private int edad;
+    private int telefono;
+    private float estatura;
     private String nombre;
-    private String apellido;
-
-
-
+    private int numero;
+    private int platos;
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Usuario)) return false;
-        Usuario usuario = (Usuario) o;
-        return getEdad() == usuario.getEdad() && Float.compare(Usuario.getNombre()) && Objects.equals(getApellido(), usuario.getApellido());
+        Usuario Usuario = (Usuario) o;
+        return getTelefono() == Usuario.getTelefono() && Float.compare(Usuario.getEstatura(), getEstatura()) == 0 && getNumero() == Usuario.getNumero() && Objects.equals(getNombre(), Usuario.getNombre()) && Objects.equals(getPlatos(), Usuario.getPlatos());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getEdad(), getNombre(), getApellido());
+        return Objects.hash(getTelefono(), getEstatura(), getNombre(), getNumero(), getPlatos());
     }
 
-    public void setNombre(String nombre) {
+    public Usuario(int telefono, float estatura, String nombre, int numero, int sueldo) {
+        this.telefono = telefono;
+        this.estatura = estatura;
         this.nombre = nombre;
+        this.numero = numero;
+        this.platos = platos;
     }
 
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
+    public int getTelefono() {
+        return telefono;
     }
 
-
-    public int getEdad() {
-        return edad;
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
     }
 
-    public void setEdad(int edad) {
-        this.edad = edad;
+    public float getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(float precio) {
+        this.estatura = estatura;
     }
 
     public String getNombre() {
         return nombre;
     }
 
-    public String getApellido() {
-        return apellido;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getPlatos() {
+        return platos;
+    }
 
     @Override
     public String toString() {
-            return "Usuario -> " +
-                "edad=" + edad +
+        return "Usuario -> " +
+                "telefono=" + telefono +
+                ", estatura=" + estatura +
                 ", nombre='" + nombre + '\'' +
-                ", apellido=" + apellido;
-
+                ", numero=" + numero +
+                ", platos=" + platos;
     }
 
-    public Usuario(int edad, String nombre, String apellido) {
-        this.edad = edad;
-        this.nombre = nombre;
-        this.apellido = apellido;
+    public void setPlatos(int platos) {
+        this.platos = platos;
     }
 }
